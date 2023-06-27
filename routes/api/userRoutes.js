92e72d3d0bab888aca6044f625382b91c5d8f8c3
Gabriel Parada <gabrielparada05@gmail.com>
2023-06-26 20:47:27 -0400

    user routes done

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  removeThought,
+  addFriend,
+  removeFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUser).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
+
+// /api/users/:userId/:thoughtId
+router.route('/:userId/:thoughtId').delete(removeThought);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+
+
+
+module.exports = router;
